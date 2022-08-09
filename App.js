5f20a1b198ec9ed087bb14eb51618f40425b982b
adityahimaone <adityahimaone@gmail.com>
2022-08-09 21:30:43 +0700

    feat: text, view, text input, button styling

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
+      <Text style={styles.title}>My App</Text>
       <View style={styles.descContainer}>
         <Text style={styles.description}>
           This is a simple React Native app that shows how to use the Expo
         </Text>
         <Text style={styles.descTips}>Learning by doing!</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <View>
+          <TextInput style={styles.input} placeholder="Your course goal!" />
+          <Button color="#F65A83" title="Add Goal" />
+        </View>
+        <View>
+          <Text>List of Goals...</Text>
+        </View>
       </View>
       <Button
         title="Press me"
@@ -44,7 +53,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: "black",
     borderWidth: 2,
-    borderRadius: 10,
+    borderRadius: 8,
     borderColor: "#FFF8BC",
     padding: 5,
     marginVertical: 8,
@@ -58,5 +67,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#F65A83",
     borderRadius: 12,
     shadowOffset: { width: 1, height: 2 },
+  },
+  inputContainer: {
+    width: "90%",
+    padding: 8,
+    marginHorizontal: 12,
+    marginVertical: 8,
+    backgroundColor: "#FFF8BC",
+    borderRadius: 12,
+    shadowOffset: { width: 1, height: 2 },
+  },
+  input: {
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: "#FFF8BC",
+    color: "#F65A83",
+    padding: 5,
+    marginVertical: 8,
+  },
+  buttonInput: {
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: "#FFF8BC",
+    color: "#F65A83",
+    padding: 5,
+    marginVertical: 8,
   },
 });
