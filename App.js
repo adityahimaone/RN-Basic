60fe42729f80fd2ef8fd8264bd62708329f3ff81
adityahimaone <adityahimaone@gmail.com>
2022-08-09 21:14:44 +0700

    feat: try component with style

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.title}>Hello World!</Text>
+      <View style={styles.descContainer}>
+        <Text style={styles.description}>
+          This is a simple React Native app that shows how to use the Expo
+        </Text>
+        <Text style={styles.descTips}>Learning by doing!</Text>
+      </View>
+      <Button
+        title="Press me"
+        color="#FF87B2"
+        onPress={() => Alert.alert("Button with adjusted color pressed")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f2f2",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    margin: 12,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF87B2",
+  },
+  description: {
+    color: "white",
+    fontSize: 16,
+  },
+  descTips: {
+    fontSize: 12,
+    color: "black",
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "#FFF8BC",
+    padding: 5,
+    marginVertical: 8,
+    textAlign: "center",
+  },
+  descContainer: {
+    padding: 8,
+    marginHorizontal: 12,
+    marginVertical: 8,
+    color: "#F65A83",
+    backgroundColor: "#F65A83",
+    borderRadius: 12,
+    shadowOffset: { width: 1, height: 2 },
   },
 });
